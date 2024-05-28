b9922fcb4d89d71c2fe4fc2744f0b1f179c3fe83
keirysramos <hadedayanajalaff@gmail.com>
2024-05-28 15:51:24 -0500

    Primera carga

---
+++ funciones.js
@@ -0,0 +1,24 @@
+
+function enviarDatos() {
+    /*var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+    
+    localStorage.setItem('email', email);
+    localStorage.setItem('message', message);*/
+    //var mensaje = "file:///D:/ing%20oscar%20gomez/analisis%20en%20la%20ma%C3%B1ana/jesu/terea2/index11.html";
+    
+    var array = [1, 2, 3];
+    var ultimo = array.push(4);
+    document.getElementById("resultado").innerHTML="ok"
+    //document.getElementById("resultado").innerHTML=` ${numeroLetras}`
+
+    /*if(numeroLetras>10){
+      document.getElementById("resultado").innerHTML=`Identificación no puede tener ams de 10 digitos ${numeroLetras}`
+
+    }else{
+      document.getElementById("resultado").innerHTML=numeroLetras
+
+    }*/
+
+    
+  }
